79132020abf40758398e1d6f4a5a45763bfde07c
Venugopal K <venu.shastri@gmail.com>
2025-09-10 22:06:59 +0530

    Update ReferenceManual.h

--- color-coding-system/inc/ReferenceManual.h
+++ color-coding-system/inc/ReferenceManual.h
@@ -6,11 +6,12 @@ namespace TelCoColorCoder
 {
     class ReferenceManual
     {
-    public:
+   private:
         static std::string generateColorCodingReference();
         static std::string formatColorPairEntry(int pairNumber, 
                                                const std::string& majorColor, 
                                                const std::string& minorColor);
-        static void printColorCodingReference();
+ public:
+        static std::string getColorCodingReference();
     };
 }
